5484621ee2b7b873a98155d1db659e67d8242b66
PolinaAzimka <timcukpolina@gmail.com>
2025-03-24 12:34:27 +0900

    Update main.py 2.3

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import pytesseract
 from PIL import Image
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
-import asyncio
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -30,14 +29,8 @@ async def handle_photo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     except Exception as e:
         await update.message.reply_text(f"Ошибка распознавания текста: {e}")
 
-async def main():
-    TOKEN = os.getenv("BOT_TOKEN")
-    application = ApplicationBuilder().token(TOKEN).build()
-
-    await application.bot.delete_webhook(drop_pending_updates=True)
-    application.add_handler(MessageHandler(filters.PHOTO, handle_photo))
-
-    await application.run_polling()
-
 if __name__ == '__main__':
-    asyncio.run(main())
+    TOKEN = os.getenv("BOT_TOKEN")
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(MessageHandler(filters.PHOTO, handle_photo))
+    app.run_polling()
